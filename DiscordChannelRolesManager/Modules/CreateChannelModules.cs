89b26989a7f5a099c0e9ea1650b136c7a4c9f1e0
Jakub Zając <jjzajac@pop-os.localdomain>
2021-06-13 02:10:39 +0200

    Add Interface and InMemory CreatedInfoService

--- DiscordChannelRolesManager/Modules/CreateChannelModules.cs
+++ DiscordChannelRolesManager/Modules/CreateChannelModules.cs
@@ -5,24 +5,35 @@ using Discord;
 using Discord.Commands;
 using Discord.Rest;
 using DiscordChannelRolesManager.Helpers.TypeReaders;
+using DiscordChannelRolesManager.Services;
 
 namespace DiscordChannelRolesManager.Modules
 {
     public class CreateChannelModules : ModuleBase<SocketCommandContext>
     {
+        private readonly IStoreCreatedInfoService _storeCreatedInfoService;
+
+        public CreateChannelModules(IStoreCreatedInfoService storeCreatedInfoService)
+        {
+            _storeCreatedInfoService = storeCreatedInfoService;
+        }
+
         [Command("create")]
         public async Task CreateChannelAsync([Remainder] IDictionary<string, string> dict)
         {
             dict.SplitOptionsParams(out var options, out var rest);
 
             ulong? catId = null;
+            var catExist = true;
             if (options.TryGetValue(":cat", out var catVal))
             {
                 var cat = Context.Guild.CategoryChannels.Where(c => c.Name == catVal).ToList();
+                catExist = cat.Any();
                 catId = cat.Any()
                         ? cat.First().Id
                         : (await Context.Guild.CreateCategoryChannelAsync(catVal)).Id;
             }
+
 
             foreach (var name in rest.Values)
             {
@@ -37,10 +48,22 @@ namespace DiscordChannelRolesManager.Modules
                 var channel = await CreateChannelWithRoleAsync(name, catId, roleId);
             }
 
-            await SendResponseWithReactions(rest);
+            var res = await SendResponseWithReactions(rest);
+            if (options.TryGetValue(":label", out var label))
+            {
+                _storeCreatedInfoService.AddCreatedInfoLabel(
+                        label, Context.Guild.Id, new CreatedInfo
+                        {
+                                Cat = catId,
+                                IsCatCreated = !catExist,
+                                ChannelId = Context.Channel.Id,
+                                MessageId = res.Id,
+                        }
+                );
+            }
         }
 
-        private async Task SendResponseWithReactions(Dictionary<string, string> rest)
+        private async Task<RestUserMessage> SendResponseWithReactions(Dictionary<string, string> rest)
         {
             string s = rest.Aggregate("", (current, e) => current + $"- {e.Key} -> {e.Value}\n");
             var b = new EmbedBuilder
@@ -53,6 +76,8 @@ namespace DiscordChannelRolesManager.Modules
             {
                 await res.AddReactionAsync(new Emoji(keyValuePair.Key));
             }
+
+            return res;
         }
 
         private async Task<RestTextChannel> CreateChannelWithRoleAsync(string name, ulong? catId, ulong roleId)
@@ -79,6 +104,13 @@ namespace DiscordChannelRolesManager.Modules
             );
         }
 
-        private CreateChannelModules() { }
+        [Command("rem")]
+        public async Task CreateChannelAsync(string label)
+        {
+            if (_storeCreatedInfoService.TryGetCreatedInfo(label, Context.Guild.Id, out var info))
+            {
+                await Context.Channel.SendMessageAsync("Test");
+            }
+        }
     }
 }
